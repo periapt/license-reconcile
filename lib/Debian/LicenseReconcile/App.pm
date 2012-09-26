5ec7a8fdbc62266bcf319d850f4574383e684d18
Nicholas Bamber <nicholas@periapt.co.uk>
2012-09-26 10:10:01 +0100

    refactored code for easier testability

--- lib/Debian/LicenseReconcile/App.pm
+++ lib/Debian/LicenseReconcile/App.pm
@@ -12,10 +12,163 @@ use Class::XSAccessor
         config_file => 'config_file',
         copyright => 'copyright',
         directory =>'directory',
-        filters_override => 'filters_override',
+        filters => 'filters',
         format_spec => 'format_spec',
     },
 ;
+use File::Slurp;
+use Readonly;
+use Debian::LicenseReconcile::Errors;
+use Debian::LicenseReconcile::FormatSpec;
+use Debian::LicenseReconcile::CopyrightTarget;
+use Debian::LicenseReconcile::LicenseCheck;
+use Debian::LicenseReconcile;
+use Parse::DebianChangelog;
+use UNIVERSAL::require;
+use Config::Any;
+
+sub _read_copyright_file {
+    my $self = shift;
+    my $copyright_text = scalar read_file($self->copyright);
+    if ($self->format_spec) {
+        Debian::LicenseReconcile::FormatSpec->check($copyright_text);
+    }
+    my $copyright_target = Debian::LicenseReconcile::CopyrightTarget->new;
+    if ($copyright_target->parse($copyright_text)) {
+        return $copyright_target;
+    }
+    return;
+}
+
+sub _parse_changelog {
+    my $self = shift;
+    return Parse::DebianChangelog->init(
+        {
+            infile=>$self->changelog_file
+        }
+    );
+}
+
+sub _parse_config {
+    my $self = shift;
+    return Config::Any->load_files({
+        files=>[$self->config_file],
+        use_ext=>1,
+        flatten_to_hash=>1,
+    })->{$self->config_file};
+}
+
+sub _build_licensecheck {
+    my $self = shift;
+    my $config = shift;
+    return Debian::LicenseReconcile::LicenseCheck->new(
+        $self->directory,
+        $config->{LicenseCheck},
+    );
+}
+
+sub _build_file_mapping {
+    my $self = shift;
+    my $copyright_target = shift;
+    my $file_mapping = $copyright_target->map_directory($self->directory);
+
+    if ($self->display_mapping) {
+        foreach my $file (keys %$file_mapping) {
+            print "$file: $file_mapping->{$file}->{pattern}\n";
+        }
+    }
+    return $file_mapping;
+}
+
+sub run {
+    my $self = shift;
+
+    Readonly my $CHANGELOG => $self->_parse_changelog;
+    Readonly my $CONFIG => $self->_parse_config;
+    Readonly my $LICENSECHECK => $self->_build_licensecheck($CONFIG);
+    Readonly my $COPYRIGHT_TARGET => $self->_read_copyright_file;
+    if ($COPYRIGHT_TARGET) {
+
+        Readonly my $FILE_MAPPING => $self->_build_file_mapping($COPYRIGHT_TARGET);
+        Readonly my $RECONCILE =>
+            Debian::LicenseReconcile->new($COPYRIGHT_TARGET->patterns);
+        my $file_checked = {};
+        foreach my $filter_name (@{$self->filters}) {
+            $file_checked = $self->_run_filter(
+                $filter_name,
+                $file_checked,
+                $CONFIG,
+                $CHANGELOG,
+                $LICENSECHECK,
+                $FILE_MAPPING,
+                $RECONCILE
+            );
+        }
+
+    }
+    return $self->_report_errors;
+}
+
+sub _run_filter {
+    my $self = shift;
+    my $filter_name = shift;
+    my $file_checked = shift;
+    my $config = shift;
+    my $changelog = shift;
+    my $licensecheck = shift;
+    my $file_mapping = shift;
+    my $reconcile = shift;
+    my $class;
+    ($class, $filter_name) = _parse_filter_name($filter_name);
+    $class->require;
+    my @files_remaining
+        = grep {not exists $file_checked->{$_}} keys %$file_mapping;
+    my $test = $class->new(
+        directory=>$self->directory,
+        files_remaining=>\@files_remaining,
+        config=>$config->{$filter_name},
+        changelog=>$changelog,
+        licensecheck=>$licensecheck,
+        name=>$filter_name,
+    );
+    foreach my $titbit ($test->get_info) {
+        next if $file_checked->{$titbit->{file}};
+        $file_checked->{$titbit->{file}} = 1;
+        if (exists $file_mapping->{$titbit->{file}}) {
+            $reconcile->check($titbit, $file_mapping->{$titbit->{file}});
+        }
+        else {
+            Debian::LicenseReconcile::Errors->push(
+                test => 'File mismatch',
+                msg => "Filter $filter_name found $titbit->{file} which was not in the file mapping. This probably implies a bug in the filter.",
+            );
+        }
+    }
+    return $file_checked;
+}
+
+sub _report_errors {
+    my $self = shift;
+    if (not $self->quiet) {
+        foreach my $error (Debian::LicenseReconcile::Errors->list) {
+            warn "$error->{test}: $error->{msg}";
+        }
+    }
+    return Debian::LicenseReconcile::Errors->how_many > 0 ? 1 : 0;
+}
+
+sub _parse_filter_name {
+    my $filter_name = shift;
+    my $class = "Debian::LicenseReconcile::Filter::";
+    if ($filter_name =~ m{\A(\w+)~(\w+)\z}xms) {
+        $class .= $2;
+        $filter_name = $1;
+    }
+    else {
+        $class .= $filter_name;
+    }
+    return $class, $filter_name;
+}
 
 =head1 NAME
 
@@ -58,7 +211,7 @@ object.
 
 =head2 directory
 
-=head2 filters_override
+=head2 filters
 
 =head2 format_spec
 
